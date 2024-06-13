3006ee0b13f26261e92e04c53e5dd14a325761ce
Chadri <114363325+chadri0@users.noreply.github.com>
2024-06-13 16:22:28 -0700

    made changes to app.js and index.js files

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./shared/Header";
 import Home from "./components/Home";
 import Login from "./components/Login";
@@ -7,15 +8,20 @@ import Signup from "./components/Signup";
 import Todolist from "./components/Todolist";
 
 
+
 function App() {
+  const [user, setUser] = useState({});
+
   return (
     <div className="App">
-      <Header />
-      <Home />
-      <Login />
-      <Settings />
-      <Signup />
-      <Todolist /> 
+      <Header user={user} setUser={setUser} />
+      <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login user={user} setUser={setUser} />} />
+      <Route path="/settings" element={<Settings />} />
+      <Route path="/signup" element={<Signup user={user} setUser={setUser} />} />
+      <Route path="/todolist" element={<Todolist />} />
+      </Routes> 
     </div>
   );
 }
